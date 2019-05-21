ea3a5db4a53aa2ecfbfdca3556f7fa2713b1e3ea
Isak Falk <me@isakfalk.com>
2019-05-21 21:18:18 +0100

    Git init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='active_learning_code',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the accompanying code repo for my active learning MRes dissertation',
+    author='Isak Falk',
+    license='MIT',
+)
