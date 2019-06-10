8129f8ca8823bca2e85aa0011ed3f15d3c9e11e9
Isak Falk <me@isakfalk.com>
2019-06-10 15:38:11 +0100

    Add utility functions (kernels mostly)

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,16 @@
+import json
+from datetime import datetime
+from pathlib import Path
+
 import numpy as np
 from scipy import linalg
 from scipy.spatial.distance import cdist, pdist, squareform
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
+
+import src.algorithms as alg
+from src.PARAMATERS import data_experiments_dir
 
 
 def gaussian_kernel_matrix(X, Y=None, s2=1.0):
@@ -62,6 +70,24 @@ def mmd2(K_xx, K_yy, K_xy, w_x=None, w_y=None):
     return mmd2
 
 
+def find_kernel_diameter(K):
+    """Find the diameter of the polytope in RKHS"""
+    n = K.shape[0]
+    best_max = 0
+    best_indices = [0, 0]
+    current_max = 0
+    current_indices = [0, 0]
+    for i in range(0, n):
+        for j in range(0, i+1):
+            current_max = K[i, i] + K[j, j] - 2*K[i, j]
+            current_indices = [i, j]
+            if current_max > best_max:
+                best_max = current_max
+                best_indices = current_indices
+
+    return best_max, best_indices
+
+
 def calculate_mmd_curve(K, sampled_order, W=None):
     """Get the whole mmd scores for the sampling strategy that produce X_sampled
 
@@ -93,44 +119,6 @@ def calculate_mmd_curve(K, sampled_order, W=None):
                                 w_y=w_y) ** 0.5
 
     return mmd_curve
-
-
-def visualise_mmd_curve(mmd_curve, loglog=True):
-    """Visualise the trace of the mmd score as a function of the size
-
-    Given a curve which has mmd_curve[t] = MMD(P, Q_t), visualise
-    both the normal plot and the log plot.
-
-    :param mmd_curve: (1 x n, np.ndarray) mmd scores
-    :param loglog: (bool) if true, plot loglog, else semilogy
-
-    :return fig, ax:"""
-    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
-
-    # first we do normal plot
-    ax[0].plot(mmd_curve, linestyle='-')
-    ax[0].set_xlabel(r't')
-    ax[0].set_ylabel(r'$MMD(\hat{P}, \hat{Q}_t)$')
-    ax[0].set_ylim([0.0, 1.0])
-    ax[0].set_title('MMD vs t')
-
-    # Then we do semilogyx to get the actual relationship
-    # NB: If we have relationships of kind MMD = O(t**-a)
-    # we will have relationships of the kind
-    # log(y) = log(C) - a log(t)
-    # If we have relationships like MMD = A exp(-O(t))
-    # we will have relationships of the kind
-    # log(y) = log(A) - C * t
-    # Choose the plot that makes sense given the method visualising
-    t = np.arange(0, n)
-    if loglog:
-        ax[1].loglog(t[0:-1], mmd_curve[0:-1], linestyle='-', marker='^')
-        ax[1].set_title('MMD vs t, loglog-plot')
-    else:
-        ax[1].semilogy(t[0:-1], mmd_curve[0:-1], linestyle='-', marker='^')
-        ax[1].set_title('MMD vs t, logy-plot')
-
-    return fig, ax
 
 
 def create_f(x_is, alpha_is, s2=1.0, normalise=True):
@@ -198,6 +186,258 @@ def get_extremum(f,
     return np.max(z), np.min(z)
 
 
+def kernel_quantile_heuristic(X, q=0.5):
+    """Calculates the optimal s2 given the dataset X
+
+    Using the quantile heurstic we calculate the best s2.
+
+    :return quantile_heuristic_s2: (float) s2 picked according to quantile heuristic"""
+    n, d = X.shape
+    pairwise_sq_dists = squareform(pdist(X, 'sqeuclidean'))
+    pairwise_sq_dists_unbiased = pairwise_sq_dists[np.triu_indices(n, k=1)]
+    quantile_heuristic_s2 = np.quantile(pairwise_sq_dists_unbiased, q=q)
+    return quantile_heuristic_s2
+
+
+def pick_optimal_params_using_cv(X, y, cv=5, iid=False):
+    """Using cross validation on dataset, pick best parameters
+
+    Using the gaussian kernel ridge regression, pick the best tau and s2
+    using cross validation on the data. Internally use the 0.05 and 0.95
+    quantile as base low and high values to linspace over.
+
+    :param X: (np.ndarray, (n, d)) design matrix
+    :param y: (np.ndarray, (n, 1)) output array
+    :param cv: (int) number of folds in cross-validation
+    :param iid: (bool) if we assume iid or not (False lead to normal k-fold cv)
+
+    :return gkr_cv: (GaussianKernelRidgeRegression) optimised gkr instance through sklearn GridSearchCV
+    :rerurn tau: (float) optimal tau for GKRR
+    :return s2: (float) optimal s2 for GKRR"""
+    gaussian_kr = GaussianKernelRidgeRegression()
+
+    q10_sq_dist = kernel_quantile_heuristic(X, q=0.05)
+    q90_sq_dist = kernel_quantile_heuristic(X, q=0.95)
+
+    param_grid = dict(
+        tau=np.logspace(-3, 1, 5),
+        s2=np.linspace(q10_sq_dist * 0.1, q90_sq_dist * 10, 5)
+    )
+
+    gkr_cv = GridSearchCV(estimator=gaussian_kr,
+                          param_grid=param_grid,
+                          scoring='neg_mean_squared_error',
+                          cv=cv, iid=iid)
+    gkr_cv.fit(X, y)
+    tau = gkr_cv.best_params_['tau']
+    s2 = gkr_cv.best_params_['s2']
+    return gkr_cv, tau, s2
+
+
+def calculate_learning_curve(K, y, sampled_order, tau, stop_t=None):
+    """Calculate learning curves from running herding algorithm
+
+    Using the sampled order from the sampled_order indexing array
+    calculate the learning curve on the train set using GKRR. Note that we
+    pass K instead of calculating it on the fly, that's why we don't use
+    s2 explicitly, it's already used in calculating K.
+
+    :param K: (np.ndarray, (n, n)) full kernel matrix from dataset
+    :param y: (np.ndarray, (n, 1)) output array
+    :param sampled_order: (np.ndarray, (n,)) order of the sampled indices
+    :param tau: (float) regularisation parameter used in GKRR
+    :param stop_t: (int) final step of calculations
+
+    :return learning_curve: (np.ndarray, (stop_t,)) array of mse for size of train set
+    """
+    gaussian_kr = GaussianKernelRidgeRegression(
+        tau=tau, s2=None, precompute_K=True)
+
+    n = K.shape[0]
+    if stop_t is None:
+        stop_t = n
+
+    all_indices = np.arange(n)
+    K_sampled = K[np.ix_(sampled_order, sampled_order)]
+
+    learning_curve = np.zeros(stop_t)
+    for t in range(stop_t):
+        K_sampled_t = K_sampled[0:t+1, 0:t+1]
+        gaussian_kr.fit(X=K_sampled_t, y=y[sampled_order[:t+1]])
+        # NB: the sampled_order index the train set
+        # and all indices the set we are predicting
+        K_xn = K[np.ix_(all_indices, sampled_order[:t+1])]
+        y_ = gaussian_kr.predict(K_xn)
+        learning_curve[t] = mean_squared_error(y, y_)
+
+    return learning_curve
+
+
+def calculate_learning_curves_train_test(K, y, train_indices, test_indices, sampled_order_train,
+                                         tau, stop_t=None):
+    """Calculate learning curves (train, test) from running herding algorithm
+
+    Using the sampled order from the sampled_order indexing array
+    calculate the learning curves on the train set using GKRR. Note that we
+    pass K instead of calculating it on the fly, that's why we don't use
+    s2 explicitly, it's already used in calculating K.
+
+    :param K: (np.ndarray, (n, n)) full kernel matrix from dataset
+    :param y: (np.ndarray, (n, 1)) output array
+    :param train_indices: (np.ndarray, (n_train,)) train indices from the original dataset
+    :param test_indices: (np.ndarray, (n_train,)) test indices from the original dataset
+    :param sampled_order_train: (np.ndarray, (n_train,)) order of the sampled training indices
+    :param tau: (float) regularisation parameter used in GKRR
+    :param stop_t: (int) final step of calculations
+
+    :return learning_curve_train: (np.ndarray, (stop_t,)) array of mse for train set
+    :return learning_curve_test: (np.ndarray, (stop_t,)) array of mse for test set
+    """
+    gaussian_kr = GaussianKernelRidgeRegression(
+        tau=tau, s2=None, precompute_K=True)
+
+    # Index K differently depending on what we do.
+    # When predicting, we need the kernel matrix to be
+    # K_mn, where m indexes the set to predict over and
+    # n indexes the set we train over
+    K_train = K[np.ix_(train_indices, train_indices)]
+    K_test = K[np.ix_(test_indices, test_indices)]
+    K_test_train = K[np.ix_(test_indices, train_indices)]
+    K_sampled_train = K_train[np.ix_(sampled_order_train, sampled_order_train)]
+
+    y_train = y[train_indices]
+    y_test = y[test_indices]
+    y_sampled_train = y_train[sampled_order_train]
+
+    n_train = K_train.shape[0]
+    n_test = K_test.shape[0]
+
+    if stop_t is None:
+        stop_t = n_train
+
+    learning_curve_train = np.zeros(stop_t)
+    learning_curve_test = np.zeros(stop_t)
+
+    for t in range(stop_t):
+        K_sampled_train_t = K_sampled_train[0:t+1, 0:t+1]
+        gaussian_kr.fit(X=K_sampled_train_t, y=y_sampled_train[:t+1])
+
+        # Predict for train set
+        K_xn_train = K_train[np.ix_(
+            np.arange(n_train), sampled_order_train[:t+1])]
+        y_train_ = gaussian_kr.predict(K_xn_train)
+        learning_curve_train[t] = mean_squared_error(y_train, y_train_)
+
+        # Then test set
+        K_xn_test = K_test_train[np.ix_(
+            np.arange(n_test), sampled_order_train[:t+1])]
+        y_test_ = gaussian_kr.predict(K_xn_test)
+        learning_curve_test[t] = mean_squared_error(y_test, y_test_)
+
+    return learning_curve_train, learning_curve_test
+
+
+def train_test_indices(n, train_ratio=0.7):
+    """Create train and test indices
+
+    Create a new random index for test and train set
+    using the train_ratio as the proportion of train to
+    test data.
+
+    :param n: (int) size of original dataset
+    :param train_ratio: (float) ratio of points that should be training data
+
+    :return train_indices: (np.ndarray) train indices
+    :return test_indices: (np.ndarray) test indices"""
+    train_split_index = int(train_ratio * n)
+    shuffled_indices = np.random.permutation(n)
+    train_indices = shuffled_indices[:train_split_index]
+    test_indices = shuffled_indices[train_split_index:]
+    return train_indices, test_indices
+
+
+def create_krr_mmd_kernel_matrices(X, s2_opt):
+    """s2_opt is the optimal s2 hyperparameter for KRR"""
+    K = gaussian_kernel_matrix(X, s2=s2_opt)
+    K_mmd = gaussian_kernel_matrix(X, s2=s2_opt/2.0)
+    return K, K_mmd
+
+
+def sample_mc_learning_curves_train_test(K, y, train_indices, test_indices, tau, num_trajectories=5):
+    """Sample learning curves when using a stochastic herding algorithm
+
+    :param sampling_algorithm: (src.alg herding algorithm) instance of kernel herding algorithm
+    """
+    mc = alg.MCSampling(K[np.ix_(train_indices, train_indices)])
+
+    learning_curves_mc_train = []
+    learning_curves_mc_test = []
+    for i in range(num_trajectories):
+        mc.run_mc()
+        lc_train, lc_test = calculate_learning_curves_train_test(K=K, y=y,
+                                                                 train_indices=train_indices,
+                                                                 test_indices=test_indices,
+                                                                 sampled_order_train=mc.sampled_order,
+                                                                 tau=tau)
+        learning_curves_mc_train.append(lc_train)
+        learning_curves_mc_test.append(lc_test)
+
+    learning_curves_mc_train = np.array(learning_curves_mc_train)
+    learning_curves_mc_test = np.array(learning_curves_mc_test)
+    return learning_curves_mc_train, learning_curves_mc_test
+
+
+def run_learning_curve_experiment(X, y, dataset_name, train_ratio=0.7):
+    """Writes learning curves to file in .npy format
+
+    Will want to change what we call in this as more algorithms are added"""
+    gkr_cv, tau_opt, s2_opt = pick_optimal_params_using_cv(X, y)
+    K, K_mmd = create_krr_mmd_kernel_matrices(X, s2_opt)
+    n = K.shape[0]
+    train_indices, test_indices = train_test_indices(
+        n, train_ratio=train_ratio)
+
+    learning_curves_mc_train, learning_curves_mc_test = sample_mc_learning_curves_train_test(
+        K, y, train_indices, test_indices, tau_opt, num_trajectories=10)
+
+    K_train = K[np.ix_(train_indices, train_indices)]
+    fw = alg.FrankWolfe(K_train)
+    fw.run_frank_wolfe()
+    learning_curve_fw_train, learning_curve_fw_test = calculate_learning_curves_train_test(K, y,
+                                                                                           train_indices,
+                                                                                           test_indices,
+                                                                                           fw.sampled_order,
+                                                                                           tau_opt)
+
+    save_dir = 'learning_curves-{}'.format(dataset_name)
+    save_dir = Path(data_experiments_dir) / save_dir
+    print(save_dir)
+    save_dir.mkdir(parents=True, exist_ok=False)
+
+    # Save all of the learning curves
+    np.save(save_dir / 'learning_curve_fw_train', learning_curve_fw_train)
+    np.save(save_dir / 'learning_curve_fw_test', learning_curve_fw_test)
+    np.save(save_dir / 'learning_curves_mc_train', learning_curves_mc_train)
+    np.save(save_dir / 'learning_curves_mc_test', learning_curves_mc_test)
+
+    # Save json file of interesting information for this particular run
+    euclidean_dist_q05 = kernel_quantile_heuristic(X, q=0.05)
+    euclidean_dist_q95 = kernel_quantile_heuristic(X, q=0.95)
+
+    param_config = {
+        'n': X.shape[0],
+        'd': X.shape[1],
+        'tau_opt_KRR': tau_opt,
+        's2_opt_KRR': s2_opt,
+        'train_ratio': train_ratio,
+        'euclidean_dist_q05': euclidean_dist_q05,
+        'euclidean_dist_q95': euclidean_dist_q95,
+        'time_created': str(datetime.now().strftime('%Y-%m-%d_%H:%M:%S'))
+    }
+
+    with open(save_dir / 'experiment_config.json', 'w') as json_file:
+        json.dump(param_config, json_file)
+
 ###############################################
 # sklearn type custom kernel ridge regression #
 ###############################################
@@ -207,26 +447,42 @@ class GaussianKernelRidgeRegression(BaseEstimator, RegressorMixin):
     """This is of the form min sum_i^n (f(x_i) - y_i)**2 + tau\|f\|_H**2
        leading to alpha = (K + tau * I)^-1 Y"""
 
-    def __init__(self, tau, s2):
+    def __init__(self, tau=None, s2=None, precompute_K=False):
         self.tau = tau
         self.s2 = s2
+        self.precompute_K = precompute_K
 
     def fit(self, X, y):
-        assert X.ndim == 2
-        assert y.ndim == 2
-        assert y.shape[0] == X.shape[0]
-        assert y.shape[1] == 1
-        self.n, self.d = X.shape
-        self.X = X
-        self.K = gaussian_kernel_matrix(X, s2=self.s2)
+        if y.ndim == 1:
+            y = y.reshape(-1, 1)
+        else:
+            assert y.shape[1] == 1, "y needs to be univariate output"
+
+        self.y = y
+        self.n = X.shape[0]
+
+        # Branch depending on if we reuse K or calculate it X
+        if self.precompute_K:
+            self.K = X
+            assert self.K.shape == (X.shape[0], X.shape[0])
+        else:
+            assert X.ndim == 2
+            assert y.shape[0] == X.shape[0]
+            self.X = X
+            self.K = gaussian_kernel_matrix(X, s2=self.s2)
         a = (self.K + self.tau * np.eye(self.n))
         self.alpha = linalg.solve(a=a, b=y, assume_a='pos')
 
     def predict(self, X):
-        K_xn = gaussian_kernel_matrix(X, self.X, s2=self.s2)
-        return K_xn @ self.alpha
+        if self.precompute_K:
+            # in this case X is the cross-kernel-matrix
+            K_xn = X
+        else:
+            K_xn = gaussian_kernel_matrix(X, self.X, s2=self.s2)
+        _y = K_xn @ self.alpha
+        return _y
 
-    def score(self, X, y=None):
+    def score(self, X, y):
         return -mean_squared_error(self.predict(X), y)
 
     def get_params(self, deep=True):
@@ -236,3 +492,38 @@ class GaussianKernelRidgeRegression(BaseEstimator, RegressorMixin):
         self.tau = tau
         self.s2 = s2
         return self
+
+
+#######################################
+# Create SVD sigma for various decays #
+#######################################
+
+def create_sigma(n, decay_func):
+    S = np.arange(1, n+1).astype(float)
+    S = decay_func(S)
+    S = np.diag(S)
+    return(S)
+
+
+def create_power_decay_sigma(n, alpha):
+    def power_decay_func(S):
+        assert S.dtype == float
+        return S ** (-alpha)
+    S = create_sigma(n, power_decay_func)
+    return S
+
+
+def create_exp_decay_sigma(n, beta):
+    def exp_decay_func(S):
+        assert S.dtype == float
+        return np.exp(-beta * S)
+    S = create_sigma(n, exp_decay_func)
+    return S
+
+
+def create_tsvd_sigma(S, th):
+    # S (n, n) matrix not array
+    diag_S = np.diag(S).copy()
+    diag_S[diag_S < th] = 0.0
+    return_S = np.diag(diag_S)
+    return return_S
