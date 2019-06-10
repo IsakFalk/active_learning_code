abe74d80765c6cbf1ec20adcec546292180a58a6
Isak Falk <me@isakfalk.com>
2019-06-10 15:38:30 +0100

    Make PARAMETERS.py work with paths (NB: path hardcoded!)

--- src/PARAMATERS.py
+++ src/PARAMATERS.py
@@ -1,7 +1,12 @@
+import os
 from pathlib import Path
 
+# can't get this shit to work
+top_dir = '/home/isak/life/references/academics/phd/academic_material/project/active_learning_code/'
+
 # Directory paths
-project_dir = str(Path('.').resolve())
-img_dir = str(Path('.').resolve() / 'reports' / 'figures')
-data_external_dir = str(Path('.').resolve() / 'data' / 'external')
-data_synthetic_dir = str(Path('.').resolve() / 'data' / 'synthetic')
+project_dir = str(Path(top_dir).resolve())
+img_dir = str(Path(top_dir).resolve() / 'reports' / 'figures')
+data_external_dir = str(Path(top_dir).resolve() / 'data' / 'external')
+data_synthetic_dir = str(Path(top_dir).resolve() / 'data' / 'synthetic')
+data_experiments_dir = str(Path(top_dir).resolve() / 'data' / 'experiments')
